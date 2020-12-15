29523b0d001f3c7c4942abf57e0be057030ec008
t5w0rd <lightning_0721@163.com>
2020-12-15 16:11:29 +0800

    prometheus

--- prometheus/install.sh
+++ prometheus/install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-helm install stable/prometheus --set server.ingress.enabled=true,server.ingress.annotations.query.ingress.annotations.'kubernetes\.io/ingress\.class'=traefik,server.ingress.'hosts[0]'=prometheus.hkdev
+helm install stable/prometheus --set server.ingress.enabled=true,server.ingress.annotations.'kubernetes\.io/ingress\.class'=traefik,server.ingress.'hosts[0]'=prometheus.hkdev
